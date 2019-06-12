74a33a6613b9127b3cb4ccce72d48af6f90c14d5
Leandro Seixas Rocha <leandro.fisica@gmail.com>
2019-06-11 23:10:25 -0300

    Add qasm backend

--- teleport.py
+++ teleport.py
@@ -7,13 +7,16 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import execute
 
-# IBM Q
-from qiskit import IBMQ
-from qiskit.providers.ibmq import least_busy
-IBMQ.load_accounts()
+# IBM Q device
+#from qiskit import IBMQ
+#from qiskit.providers.ibmq import least_busy
+#IBMQ.load_accounts()
+#large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits > 3 and not x.configuration().simulator)
+#backend = least_busy(large_enough_devices)
 
-large_enough_devices = IBMQ.backends(filters=lambda x: x.configuration().n_qubits > 3 and not x.configuration().simulator)
-backend = least_busy(large_enough_devices)
-print(backend)
+# QASM simulator
+from qiskit import Aer
+backend = Aer.get_backend('qasm_simulator')
 
+print("Backend:",backend)
 
